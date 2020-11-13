4cba8ca7010d5391d5585fd0a126162c73866615
podroja17 <podroja17@sps-prosek.cz>
2020-11-13 10:38:51 +0100

    WIP wifi list

--- SensorsApp/MainPage.xaml.cs
+++ SensorsApp/MainPage.xaml.cs
@@ -4,15 +4,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SensorsApp.Util;
 using Xamarin.Forms;
 
 namespace SensorsApp
 {
     public partial class MainPage : ContentPage
     {
+        private IWifi wifi;
+
         public MainPage()
         {
             InitializeComponent();
+            this.wifi = DependencyService.Get<IWifi>();
         }
     }
 }
