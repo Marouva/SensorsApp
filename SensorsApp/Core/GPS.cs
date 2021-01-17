a9b81e73cd4173a34e3f9ac3a39c79b427aac6f4
Václav Maroušek <vaclav.marousek@gmail.com>
2021-01-17 21:10:42 +0100

    GPS Speed fix

--- SensorsApp/Core/GPS.cs
+++ SensorsApp/Core/GPS.cs
@@ -13,18 +13,46 @@ namespace SensorsApp.Core
         private static double velocity;
         private static double latitude;
         private static double longtitude;
+        private static double lastLatitude;
+        private static double lastLongtitude;
+
+        private static DateTime lastSampleTime = new DateTime();
 
         public static async Task Update()
         {
             try
             {
+                lastLongtitude = longtitude;
+                lastLatitude = latitude;
+
                 GeolocationRequest request  = new GeolocationRequest(GeolocationAccuracy.Best);
                 Location           location = await Geolocation.GetLocationAsync(request);
 
-                velocity   = location.Speed.GetValueOrDefault();
                 latitude   = location.Latitude;
                 longtitude = location.Longitude;
-                //return true;
+
+                //velocity = location.Speed.GetValueOrDefault();
+
+                /*
+                 * Xamarin je tragédie
+                 * location.speed nefunguje, proto musíme počítat manuálně
+                 */
+                const double earthRadius = 6378.137; // Radius of earth in KM
+
+                double diffLattitude = (latitude * Math.PI / 180) - (lastLatitude * Math.PI / 180);
+                double diffLongtitude = (latitude * Math.PI / 180) - (lastLatitude * Math.PI / 180);
+
+                double angle = Math.Sin(diffLattitude / 2) * Math.Sin(diffLattitude / 2) +
+                               Math.Cos(lastLatitude * Math.PI / 180) * Math.Cos(latitude * Math.PI / 180) *
+                               Math.Sin(diffLongtitude / 2) * Math.Sin(diffLongtitude / 2);
+
+                double curve = 2 * Math.Atan2(Math.Sqrt(angle), Math.Sqrt(1 - angle));
+                double distance = earthRadius * curve;
+
+                velocity = (distance * 1000) / (DateTime.Now - lastSampleTime).TotalSeconds; // meters
+
+
+                lastSampleTime = DateTime.Now;
             }
             catch (Exception e)
             {
